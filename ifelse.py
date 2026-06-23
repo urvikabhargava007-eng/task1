e678547d73907b5ba0fa6901e2896ed8f15b42ca
urvikabhargava <urvikabhargava007@gmail.com>
2026-06-23 14:57:29 +0530

    if/else examples

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+name = str(input( " Enter your name: " ))
+age = int(input( " Enter your age: " ))
+if age >= 18:
+ print("You are eligible to vote! ")
+else:
+ print(" Sorry, you are not eligible for voting! ")
+ 
+score = float(input( " Enter your score: " ))
+passing_marks = 50
+if score >= passing_marks:
+ print( " Congratulations, you passed! " )
+else:
+ print ( " You are Fail, you can retake the test! " )
+ 
