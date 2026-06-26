c66095fcceb5dc351f8ff3aa628b30b03ec12d14
urvikabhargava <urvikabhargava007@gmail.com>
2026-06-26 15:55:09 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,27 @@
+# counter = 5
+# while counter >= 0:
+#  counter = counter -1
+#  print(counter)
+
+# counter = 5
+# while counter >= 0 :
+#  print(counter)
+#  counter = counter - 1
+
+# counter = 1
+# while counter <= 10:
+#  print(counter)
+#  counter = counter + 2
+
+# while True:
+#     user_input = input("Enter a number:" )
+#     number = int(user_input)
+#     if number == 0:
+#         print("Loop stopped")
+#         break
+#     print(f"You entered : {number}")
+
+
+
+
+
