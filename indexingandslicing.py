7d3825abe1818b9ad883ffa5327d1b027b6f40e1
urvikabhargava <urvikabhargava007@gmail.com>
2026-06-30 14:59:57 +0530

    practice

---
+++ indexingandslicing.py
@@ -0,0 +1,46 @@
+# my_string = "hello world"
+# print(my_string)
+
+# print(my_string[5])
+# print(my_string[-3])
+# print(my_string[-1])
+#print(my_string[50])
+
+# [ : ] -> It will show complete String
+# [ start : ] -> It will start the slicing from the start index and will print the complete string
+# [ : end ] -> It will start printing the string from 0 index and till end - 1
+# [ start : end ] -> It will start printing the string from the start index and till the end - 1.
+
+# [from:to-1:in_what_order]
+
+# my_string = "Hello"
+
+# print( my_string[ 1: ] )
+# print(my_string[ :3 ])  
+# print(my_string[ 1:4 ]) 
+# print(my_string[ -3: -1 ]) 
+
+# [ start : end : difference ]
+
+# [ :: ] -> Cut the string from 0 to end by diff of one
+# [ :: 1 ] -> Cut the string from 0 to end by diff of one
+# [ :: 2 ] -> Cut the string from 0 to end by diff of two
+# [ :: -1 ] -> If difference is -v it will start the slicing from the last
+# [ start : end : diff ] -> This will start slicing from start index and go till end - 1 with a diff of diff
+
+# my_string = "0123456789"
+
+# print( my_string [  : 8 : 2 ]  )
+# print( my_string [ -9 : -2 : 2 ]  )
+
+s = "PythonProgramming"
+#extract python using slicing
+print(s[:6])
+#extract progrmming from the same string
+print(s[-11:])
+#extract the first 5 characters of a string
+print(s[:5])
+#extract the last 4 characters of a string
+print(s[-4:])
+#extract all characters except the first and last
+print(s[1:-1])
