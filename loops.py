cbddcd05b6904ac7decadea37dc1e24338119982
urvikabhargava <urvikabhargava007@gmail.com>
2026-07-03 16:53:39 +0530

    Add files via upload

--- loops.py
+++ loops.py
@@ -21,7 +21,50 @@
 #         break
 #     print(f"You entered : {number}")
 
+#creating strings
+# single quote: ' hello '
+# double quote: " hello "
+# triple quote: ''' hello '''
+
+# a = "Hello my name is Urvika"
+# b = "I am from Jaipur"
+
+# a = """ my name is urvika
+# i am from jaipur
+# i am learning python
+# i want to be a devops engineer"""
+# print(a)
+
+""" these 3 quotes also comments the strings."""
+
+#Write a program to print each character of a string on a new line using a loop.
+# a = "python"
+# index = 0
+# while index < len(a):
+#  print(a[index])
+#  index +=1
+
+#Write a program to reverse a given string using a for loop.
+# a = "python"
+# for 
 
 
 
+#Write a program to count the total number of vowels present in a string using a loop.
+# a= "Learning python is a fun"
+# counter=0
+# for letter in a:
+#  if letter.lower() in "aeiou":
+#   counter += 1
+# print("original text:", a)
+# print("total number of vowels:", counter)
+
+#Write a program to find and print the total number of spaces inside a string.
+a= "Learning python is a fun"
+counter=0
+for letter in a:
+ if letter == " ":
+  counter +=1
+print("original text:", a)
+print("total number of spaces:", counter)
 
