b7691c7c639861d636871a2acaf29299c404a72f
urvikabhargava <urvikabhargava007@gmail.com>
2026-07-22 11:57:22 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,17 @@
+def odd_even_check():
+    a = int(input("Enter a number:"))
+    if a % 2 == 0:
+            print("even")   
+    else:
+        print("odd")  
+odd_even_check()
+
+
+def div_checker_by_3():
+ a = int(input("Enter a number:"))
+ if a % 3 ==0:
+    print("Yes it is divisible by 3")
+ else:
+    print("No it is not divisible by 3")
+div_checker_by_3()
+
