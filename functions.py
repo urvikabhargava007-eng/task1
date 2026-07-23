00f6ef2d839fe43f3b79cc8700a3f368d608e0b3
urvikabhargava <urvikabhargava007@gmail.com>
2026-07-23 16:07:34 +0530

    QNA

--- functions.py
+++ functions.py
@@ -1,17 +1,150 @@
-def odd_even_check():
-    a = int(input("Enter a number:"))
-    if a % 2 == 0:
-            print("even")   
-    else:
-        print("odd")  
-odd_even_check()
+# def odd_even_check():
+#     a = int(input("Enter a number:"))
+#     if a % 2 == 0:
+#             print("even")   
+#     else:
+#         print("odd")  
+# odd_even_check()
 
 
-def div_checker_by_3():
- a = int(input("Enter a number:"))
- if a % 3 ==0:
-    print("Yes it is divisible by 3")
- else:
-    print("No it is not divisible by 3")
-div_checker_by_3()
+# def div_checker_by_3():
+#  a = int(input("Enter a number:"))
+#  if a % 3 ==0:
+#     print("Yes it is divisible by 3")
+#  else:
+#     print("No it is not divisible by 3")
+# div_checker_by_3()
+
+# def add(a,b):
+#     print(a+b)
+# def sub(a,b):
+#     print(a-b)
+# def mul(a,b):
+#     print(a*b)
+# def div(a,b):
+#     print(a/b)
+
+# a = int(input("Enter a number:"))
+# b = int(input("Enter b number:"))
+
+# add(a,b)
+# sub(a,b)
+# mul(a,b)
+# div(a,b)
+
+# def odd_even_check(a,b,c,d,e):
+# docstring
+#  "this function lets you to check wheather the number is odd or even."
+#  numbers = [a,b,c,d,e]
+#  for num in numbers:
+#      if num  % 2 == 0:
+#             print("even")   
+#      else:
+#         print("odd")  
+# a = int(input("Enter a number:"))
+# b = int(input("Enter a number:"))
+# c = int(input("Enter a number:"))
+# d = int(input("Enter a number:"))
+# e = int(input("Enter a number:"))
+# odd_even_check(a,b,c,d,e)
+
+# a = int(input("Enter 1st number:"))
+# b = int(input("Enter 2nd number:"))
+# numbers = [a,b]
+
+# 1. Print Sum of Elements
+# def print_sum_of_elements(numbers):
+#   total_sum = sum(numbers)
+    
+#   print("sum of elements:" , total_sum)
+# my_list = [10, 20, 30, 40, 50]
+# print_sum_of_elements(my_list)
+
+# 2. Print Largest Number
+# def print_largest(numbers):
+#     if numbers:
+#         largest = max(numbers)
+#         print("2. Largest:", largest)
+# my_list = [10, 20, 30, 40, 50]
+# print_largest(my_list)
+
+# 3. Print Smallest Number
+# def print_smallest(numbers):
+#     if numbers:
+#         smallest = min(numbers)
+#         print("3. Smallest:", smallest)
+# my_list = [10, 20, 30, 40, 50]
+# print_smallest(my_list)
+
+# 4. Search for an Element
+#def search_element():
+
+
+
+
+
+#5. Count Even Numbers
+# def count_evens(numbers):
+#     evens = sum(1 for x in numbers if x % 2 == 0)
+#     print("5. Even count:", evens)
+# my_list = [10, 20, 30, 40, 50]
+# count_evens(my_list)
+
+# 6. Count Odd Numbers
+# def count_odds(numbers):
+#     odds = sum(1 for x in numbers if x % 2 != 0)
+#     print("6. Odd count:", odds)
+# my_list = [10, 20, 30, 40, 50]
+# count_odds(my_list)
+
+# 7. Print Average
+def print_average(numbers):
+    if numbers:
+        avg = sum(numbers) / len(numbers)
+        print("7. Average:", avg)
+my_list = [10, 20, 30, 40, 50]
+print_average(my_list)
+
+# 8. Count Positives and Negatives
+#def count_positives_negatives(numbers):
+   
+
+# 9. Count Vowels
+#def count_vowels(text):
+    
+
+# 10. Check Palindrome
+#def check_palindrome(text):
+    
+
+# 11. Print Numbers Divisible by 5
+# def div_checker_by_5():
+#      a = int(input("Enter a number:"))
+#      if a % 5 ==0:
+#       print("Yes it is divisible by 5")
+#      else:
+#       print("No it is not divisible by 5")
+# div_checker_by_5()
+
+# 12. Print Common Elements
+#def print_common_elements(list1, list2):
+
+
+    
+# 13. Print Factorial of Each Element
+#def print_factorials(numbers):
+
+
+   
+# 14. Print Prime Numbers
+#def is_prime(n):
+
+
+    
+
+
+    
+
+
+
 
