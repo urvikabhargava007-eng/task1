1536d1363f2e7ddd38710afc8727a381dbdd45e9
urvikabhargava <urvikabhargava007@gmail.com>
2026-07-28 16:08:19 +0530

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,31 @@
+# f = open('onedrive/desktop/pythonprojects/abc.txt')
+# print(f.read())
+# f.close()
+
+# f = open('onedrive/desktop/pythonprojects/abc.txt', 'w')
+# print(f.write("I love my Country"))
+# f.close()
+# f = open('onedrive/desktop/pythonprojects/abc.txt')
+# print(f.read())
+# f.close()
+
+f = open('onedrive/desktop/pythonprojects/abc.txt')
+print(f.readlines())
+f.close()
+
+f = open('onedrive/desktop/pythonprojects/abc.txt')
+print(f.readable())
+f.close()
+
+f = open('onedrive/desktop/pythonprojects/abc.txt')
+print(f.readline())
+f.close()
+
+
+
+
+
+
+
+
+
