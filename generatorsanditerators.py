5e1a8a8e78c423d7d449eb53c0911b68495f202a
urvikabhargava <urvikabhargava007@gmail.com>
2026-09-01 16:34:38 +0530

    Add files via upload

---
+++ generatorsanditerators.py
@@ -0,0 +1,22 @@
+# def test():
+#     return 1
+#     return 2
+#     return 3
+#     return 4
+# print(test())
+# print(test())
+# print(test())
+# print(test())
+
+# def test():
+#     yield 1
+#     yield 2
+#     yield 3
+#     yield 4
+
+# a = test()
+# print( next(a))
+# print( next(a))
+# print( next(a))
+# print( next(a))
+
